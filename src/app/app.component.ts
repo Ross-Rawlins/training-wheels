de6ea2f98e7a1d331050d19db230fb81bc0c3e0f
admin-HP\admin <keegan.stephenson@engagementfactory.com>
2018-02-07 16:24:34 +0200

    Updated tto allow social & video pages to be viewed in app & added a menu icon to both

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { SocialPage } from '../pages/social/social';
+import { VideoPage } from '../pages/video/video';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +24,9 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      { title: 'Social', component: SocialPage },
+      { title: 'Video', component: VideoPage }
     ];
 
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
-
+import { SocialPage } from '../pages/social/social';
+import { VideoPage } from '../pages/video/video';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -13,7 +14,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    SocialPage,
+    VideoPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +26,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    SocialPage,
+    VideoPage
   ],
   providers: [
     StatusBar,
